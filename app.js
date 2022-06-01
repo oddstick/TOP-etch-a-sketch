2f0ea97d138af6c5f9fe9c655ba10fdcb95a3887
Cael Neary <cael.neary@gmail.com>
2022-06-01 15:00:08 -0500

    created HTML skeleton, added styles to container div for visability on webpage, started sketching out JS pseudocode

---
+++ app.js
@@ -0,0 +1,23 @@
+// experimenting with dynamically creating just one div
+// const gridTile = document.createElement("div");  
+// gridTile.setAttribute("style", "height: 250px");
+// document.getElementById("grid-container").appendChild(gridTile);
+
+function main() {
+    const i = gridSize;
+
+    for (i = 1; i <= 100; i++) {
+        //call function that prompts user for i (grid is square, so height x width are the same #)
+        //take user input (i) and create grid that is "i" tiles long and "i" tiles wide
+        // do this by calling a function which creates 1 tile "i" times long and "i" times wide
+        // should/can this be done using an array? create an array of divs??
+        // can you give ids/classes to elements created dynamically with JS?
+    };
+};
+
+// function prompts user for grid size returns "gridSize"
+
+// function creates a grid "i" tiles long x "i" tiles wide ??
+    // calls function that creates singular tile
+
+// function creates singular tile
